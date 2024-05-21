0a97c899078ffdc27027b2335f05c831670a5d80
faithfuldao <victor.dubrana2@gmail.com>
2024-05-21 18:51:14 +0200

    small change

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,18 +1,32 @@
 import React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 import userIcon from "../../../public/userIcon.svg";
-import groupNumberIcon from "../../../public/groupNumberIcon.svg";
 
-export default function Events() {
+export interface EventsProps {
+  eventName: string;
+  eventImage: StaticImageData | undefined;
+  date: string;
+}
+
+export default function Events(props: EventsProps) {
   const Event = () => {
     return (
       <div className=" flex flex-row my-1 bg-inputField-background rounded-md border-2 border-componentOutline w-5/6 h-24  justify-center items-center">
         <div className="flex flex-row  w-full p-1 ">
-          <Image alt="userIcon" src={userIcon} height={40} width={40} />
+          {props.eventImage ? (
+            <Image
+              alt="userIcon"
+              src={props.eventImage}
+              height={40}
+              width={40}
+            />
+          ) : (
+            <Image alt="userIcon" src={userIcon} height={40} width={40} />
+          )}
           <div className="flex flex-col text-subTitle items-left">
-            <div className="ml-2 text-white">Highschool Gathering</div>
-            <div className="flex flex-row ml-2">3rd June</div>
+            <div className="ml-2 text-white">{props.eventName}</div>
+            <div className="flex flex-row ml-2">{props.date}</div>
           </div>
         </div>
         <div className="bg-btn-background text-subTitle m-2 w-3/6">
