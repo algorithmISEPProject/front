d81a4aa7dd5bec94dd4cadac2d5a7c686f50a21b
faithfuldao <victor.dubrana2@gmail.com>
2024-05-24 13:50:03 +0200

    changes

--- src/components/profile/profileEditModal.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import userIcon from "../../../public/userIcon.svg";
-
-interface ProfileEditModalProps {}
-
-export default function ProfileEditModal() {
-  return <div>Bonjour</div>;
-}
--- src/components/profile/profileInfoEditModal.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ProfileInfoEditModal() {
-  return <div>Bonjour</div>;
-}
