7af407f6e44e15de0a3d7a9c60a255bf99aca64d
faithfuldao <victor.dubrana2@gmail.com>
2024-05-26 13:02:49 +0200

    notifications page

--- src/pages/notifications/components/friend.tsx
+++ src/pages/notifications/components/friend.tsx
@@ -15,25 +15,22 @@ export interface FriendProps {
 export default function Friend(props: FriendProps) {
   return (
     <div>
-      <div className="flex flex-col bg-componentBackground my-2 p-2 rounded-md border-2 border-componentOutline text-subTitle">
-        <div className="flex flex-row items-center m-2">
-          <div className="flex flex-row space-x-2 items-center w-full">
-            {props.userImage ? (
-              <Image
-                alt="userIcon"
-                src={props.userImage}
-                height={40}
-                width={40}
-              />
-            ) : (
-              <Image alt="userIcon" src={userIcon} height={40} width={40} />
-            )}
+      <div className="flex flex-col bg-componentBackground gap-5 p-5 rounded-xl border-1 border-componentOutline text-subTitle">
+        <div className="flex items-center gap-5">
+          <div className="flex space-x-2 items-center w-full">
+            <Image
+              alt="userIcon"
+              src={props?.userImage || ""}
+              height={40}
+              width={40}
+            />
+
             <div className="text-white">{props.userName}</div>
             <div>became your friend</div>
           </div>
-          <button className="right-0 mr-2">
-            <Image alt="moreIcon" src={friendIcon} />
-          </button>
+          <div className="">
+            <Image alt="moreIcon" src={friendIcon} width={30} height={30} />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Like from "@/pages/notifications/components/like";
+import Follow from "@/pages/notifications/components/follow";
+import Friend from "@/pages/notifications/components/friend";
+import Reply from "@/pages/notifications/components/reply";
+
 function NotificationsPage() {
-  return <div>NotificationsPage</div>;
+  return (
+    <div>
+      <div className="w-full flex justify-center">
+        <div className="space-y-5 w-4/6">
+          <Like />
+          <Follow />
+          <Friend />
+          <Follow />
+          <Like />
+          <Reply />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotificationsPage;
